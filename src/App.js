5ee2b9480d1ef8da27212ccc35ada0331dd41d49
Valerii Vasin <valerii.vasin@gmail.com>
2016-10-13 13:43:17 +0200

    Add overview table

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
+      interest: 0.05,
       price: {
         D: 400,
         C: 1700,
@@ -40,7 +41,7 @@ class App extends Component {
     this.input.focus();
   }
 
-  renderTable = () => {
+  renderDetailsTable = () => {
     if (this.state.items.length === 0) {
       return null;
     }
@@ -83,6 +84,61 @@ class App extends Component {
     );
   }
 
+  renderOverviewTable() {
+    const items = this.state.items;
+
+    if (items.length === 0) {
+      return null;
+    }
+
+    const results = items.reduce((acc, item) => {
+      const itemsAmount = item.amount;
+      const cryAmount = item.crystals.amount;
+      const cryRank = item.crystals.rank;
+
+      const cryTotal = cryAmount * itemsAmount;
+      acc[cryRank] = (acc[cryRank] || 0) + cryTotal;
+
+      return acc;
+    }, {});
+
+    const availableRanks = Object.keys(results).sort();
+
+    const headers = (
+      <tr>
+        <th>cry</th>
+        <th>amount</th>
+        <th>price per cry</th>
+        <th>price total</th>
+        <th>return back</th>
+      </tr>
+    );
+
+    const rows = availableRanks.map(rank => {
+      const amount = results[rank];
+      const price = this.state.price[rank];
+
+      return (
+        <tr key={rank}>
+          <td>{rank}x</td>
+          <td>{amount}</td>
+          <td>{price}</td>
+          <td>{price * amount}</td>
+          <td>{Math.floor((1 - this.state.interest) * amount)}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <table className="table">
+        <tbody>
+          {headers}
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -94,8 +150,14 @@ class App extends Component {
             />
         </form>
 
-        <div className="crystals-table">
-          {this.renderTable()}
+        <div className="overview-table">
+          <h1>Overview</h1>
+          {this.renderOverviewTable()}
+        </div>
+
+        <div className="details-table">
+          <h1>Details</h1>
+          {this.renderDetailsTable()}
         </div>
       </div>
     );
