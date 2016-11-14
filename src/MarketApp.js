8415e8e7c93098d22199042b97737652a255fcfe
Valerii Vasin <valerii.vasin@gmail.com>
2016-11-14 18:49:42 +0100

    Refactor marketing app to few components

--- src/MarketApp.js
+++ src/MarketApp.js
@@ -13,6 +13,45 @@ const config = {
 
 firebase.initializeApp(config);
 
+const PriceRow = ({ price, amount, time }) => <tr>
+  <td>{price}</td>
+  <td>{amount}</td>
+  <td>{new Date(time).toString()}</td>
+</tr>;
+
+const PriceTable = ({ prices, limit }) => <table className="table">
+  <tbody>
+    {
+      prices.slice(0, limit ? limit : prices.length - 1).map((price, index) => <PriceRow
+        key={`${index}:${price.amount}:${price.price}`}
+        price={price.price}
+        amount={price.amount}
+        time={price.time}
+      />)
+    }
+  </tbody>
+</table>;
+
+/** One item monitoring */
+const MonitoringItem = ({ item }) => {
+  const sells = item.sell.filter(price => price.fresh).sort((a, b) => a.price - b.price);
+  const buys = item.buy.filter(price => price.fresh).sort((a, b) => b.price - a.price);
+
+  return <div className="row">
+    <h4>
+      <a href={`http://l2on.net/en/?c=market&a=item&id=${item.id}`} target="_blank">{item.name}</a>
+    </h4>
+    <div className="row">
+      <div className="col-xs-6">
+        <PriceTable prices={sells} limit={3} />
+      </div>
+      <div className="col-xs-6">
+        <PriceTable prices={buys} limit={3} />
+      </div>
+    </div>
+  </div>;
+};
+
 export class MarketApp extends Component {
   constructor() {
     super();
@@ -36,32 +75,8 @@ export class MarketApp extends Component {
       return <h1>Hello on l2 market side!</h1>;
     }
 
-    const renderPrices = prices => prices.slice(0, 5).map(price => {
-      return <tr className="row" key={`${price.amount}:${price.price}`}>
-        <td>{price.price}</td>
-        <td>{price.amount}</td>
-        <td>{new Date(price.time).toString()}</td>
-      </tr>;
-    });
+    const rows = ids.map(id => <MonitoringItem key={id} item={this.state.items[id]} />);
 
-    const rows = ids.map(id => {
-      const item = this.state.items[id];
-      const prices = item.sell.filter(price => price.fresh).sort((a, b) => a.price - b.price);
-
-      return <table key={id} className="table">
-        <thead>
-          <tr>
-            <th colSpan="3">
-            <a href={`http://l2on.net/en/?c=market&a=item&id=${id}`} target="_blank">{item.name}</a>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {renderPrices(prices)}
-        </tbody>
-      </table>;
-    });
-
-    return <div>{rows}</div>;
+    return <div className="container">{rows}</div>;
   }
 }
