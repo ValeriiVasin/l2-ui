0123be144cc15d0b8932821d8e25cd5f0ec058be
Valerii Vasin <valerii.vasin@gmail.com>
2017-04-06 14:29:08 +0200

    Add loading to crystals app

--- src/containers/crystals/crystals.js
+++ src/containers/crystals/crystals.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Controls from './controls';
 import DetailsTableContainer from './details-table';
 import OverviewTableContainer from './overview-table';
 
-export default () => {
+import { Spinner } from '../../components';
+
+const CrystalsApp = ({ loading }) => {
   return <div className="u-padding">
     <Controls />
+    { loading ? <Spinner /> : null }
     <OverviewTableContainer />
     <DetailsTableContainer />
   </div>;
 };
+
+const CrystalsAppContainer = connect(
+  state => ({ loading: state.crystals.loading }),
+)(CrystalsApp);
+
+
+export default CrystalsAppContainer;
